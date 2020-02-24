d5b409327f053a7b308c5d7a04caea9f12843333
sogomi <sogomib@gmail.com>
2020-02-24 09:16:04 +0300

    Add what we do descriptions on the icons

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+$ (document).ready(function(){
+    $ (".click").click(function(){
+    $ (".hid").toggle();
+    });
+});
+$ (document).ready(function(){
+    $ (".press").click(function(){
+    $ (".hide").toggle();
+    });
+})
+$ (document).ready(function(){
+    $ (".tap").click(function(){
+    $ (".hidden").toggle();
+    });
+})
